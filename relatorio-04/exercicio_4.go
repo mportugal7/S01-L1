17a126e5ac02541382e5810793c2711588f683ff
Mariana Novais Portugal <mariananportugal@gmail.com>
2026-09-10 19:18:46 -0300

    Add ingresso validation for VIP access

---
+++ relatorio-04/exercicio_4.go
@@ -0,0 +1,31 @@
+package main
+import "fmt"
+
+func validarIngresso(setor string, codigo int) bool {
+	if setor == "VIP" && codigo == 2026 {
+		return true
+	} else {
+		return false
+	}
+}
+func main() {
+	for {
+		var setor string
+		var codigo int
+
+		fmt.Print("Digite o setor do ingresso: ")
+		fmt.Scanln(&setor)
+
+		fmt.Print("Digite o codigo do ingresso: ")
+		fmt.Scanln(&codigo)
+
+		valido := validarIngresso(setor, codigo)
+
+		if valido {
+			fmt.Println("Acesso liberado a area VIP!")
+			break
+		} else {
+			fmt.Println("Ingresso ou setor invalido. Tente novamente.")
+		}
+	}
+}
